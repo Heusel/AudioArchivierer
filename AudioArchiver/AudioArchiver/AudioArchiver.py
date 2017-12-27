224cc4adfc6adbeafbab29bcacfc6801aaa340df
johannes <6493141+Heusel@users.noreply.github.com>
2017-12-27 19:16:47 +0100

    besseres Fehlerhandling

--- AudioArchiver/AudioArchiver/AudioArchiver.py
+++ AudioArchiver/AudioArchiver/AudioArchiver.py
@@ -2,6 +2,7 @@ import os, shutil, time
 import locale, platform
 import subprocess
 from subprocess import Popen
+import sys
 
 print ("AudioArchiver V0.2.0");
 sys = platform.system()
@@ -10,8 +11,9 @@ if sys  == 'Windows':
   print("  running on windows system")
   
   locale.setlocale(locale.LC_TIME, 'deu_deu')
-
-  basePath ="../../BeispielAufnahme/"
+  
+  # basePath = "../../BeispielAufnahme/"
+  basePath = "f:\\"
   ffmpegPath = "../ffmpeg/bin/ffmpeg.exe"
   
 if sys == 'Linux':
@@ -20,25 +22,30 @@ if sys == 'Linux':
   locale.setlocale(locale.LC_TIME, 'de_DE')
 
   basePath ="/media/pi/GODIARCHIV/"
-  ffmpegPath = " "
+  ffmpegPath = "ffmpeg"
+
 
 print ("prepairing vars")
 
 srcFile = basePath + "AHQU/USBREC/QU-ST001.WAV"
-srcFileRename =  basePath + "AHQU/USBREC/" +  time.strftime("%d.%m.%Y") + ".WAV"
-destPath = basePath +  "Gottesdienst_Archiv/" + time.strftime("%Y/%B/%d.%m.%Y/")
-destFile = destPath + time.strftime("%d.%m.%Y") + ".mp3"
+srcFileRename =  basePath + "AHQU/USBREC/" +  time.strftime("%d_%m_%Y") + ".WAV"
+destPath = basePath +  "Gottesdienst_Archiv/" + time.strftime("%Y/%B/")
+destFile = destPath + time.strftime("Gottesdienst_%d_%m_%Y") + ".mp3"
 
 
 if not os.path.isdir(destPath):
   print ("creating directories")
   os.makedirs(destPath)
 
+  
+if not os.path.isfile(srcFile):
+    print("error input file was not found")
+    exit(1)
 
 print ("normalize and convert to mp3")
 print ("please wait")
-
-cmd = ffmpegPath + " -i " + srcFile + " -vn -ar 44100 -ac 2 -filter:a loudnorm -ab 192k -f mp3 " + destFile
+meta =  '-metadata title="' + time.strftime("Gottesdienst vom %d.%m.%Y") + '" -metadata album="Martinskirche Oeschingen" -metadata copyright="Evangelische Kirchengemeinde Oeschingen"'
+cmd = ffmpegPath + " -i " + srcFile + " -vn -ar 44100 -ac 2 " + meta + " -filter:a loudnorm -ab 160k -f mp3 " + destFile
  
 proc = Popen(cmd, shell=False)
 proc.wait()
@@ -53,5 +60,7 @@ if os.path.isfile(destFile):
 
   os.rename(srcFile, srcFileRename)
   print ("done")
+  exit(0)
 else:
   print ("error output file was not found")     
+  exit(2)
